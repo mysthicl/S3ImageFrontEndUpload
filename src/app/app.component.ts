a2c918fee68a1c44bce6f2adb8744b0d102cf146
mysthicl <irvingmachado08@gmail.com>
2025-04-12 14:02:08 -0600

    First commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
 
 @Component({
   selector: 'app-root',
@@ -7,4 +8,44 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 's3-image-upload';
+  selectedFile: File | null = null;
+  imageUrl: string | null = null;
+  uploadSuccess = false;
+  uploadError = false;
+  
+  constructor(private http: HttpClient) {}
+
+  onFileSelected(event: Event): void {
+    const input = event.target as HTMLInputElement;
+    if (input.files?.length) {
+      this.selectedFile = input.files[0];
+    }
+  }
+
+  onSubmit(event: Event): void {
+    event.preventDefault();
+    if (!this.selectedFile) return;
+  
+    const formData = new FormData();
+    formData.append('image', this.selectedFile);
+  
+    this.http.post<any>('https://localhost:7232/api/S3', formData).subscribe({
+      next: (res) => {
+        if (res.success) {
+          this.imageUrl = res.data.url;
+          this.uploadSuccess = true;
+          this.uploadError = false;
+        } else {
+          this.uploadSuccess = false;
+          this.uploadError = true;
+        }
+      },
+      error: (err) => {
+        console.error(err);
+        this.uploadSuccess = false;
+        this.uploadError = true;
+      },
+    });
+  }
+  
 }
